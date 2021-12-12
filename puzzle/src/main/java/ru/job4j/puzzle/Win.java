27ab9598f6a56a16938aedfada7728d897268879
serg383834 <serg383834@yandex.ru>
2021-12-12 18:30:23 +0300

    2. Сокобан. Тестовое задание. Через count. [#75283 #9714]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -6,32 +6,26 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = true;
-        for (int[] ints : board) {
-            rsl = true;
-            for (int cell = 0; cell < board.length; cell++) {
-                if (ints[cell] != 1) {
-                    rsl = false;
-                    break;
-                }
-            }
-            if (rsl) {
-                break;
-            }
-        }
-        if (!rsl) {
-            for (int row = 0; row < board.length; row++) {
-                rsl = true;
-                for (int cell = 0; cell < board.length; cell++) {
-                    if (board[cell][row] != 1) {
+        int count = 0;
+
+        for (int cell = 0; cell < board.length; cell++) {
+            if (board[cell][count] == 1 || board[count][cell] == 1) {
+                while (count < board.length) {
+                    if(board[cell][count] == 1 ) {
+                        count++;
+                    }
+                    else if (board[count][cell] == 1 ) {
+                        count++;
+                    } else {
+                        count = 0;
                         rsl = false;
                         break;
                     }
                 }
-                if (rsl) {
+                if (count == 5) {
                     break;
                 }
             }
-
         }
         return rsl;
     }
